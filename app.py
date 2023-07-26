fe31a9c7330361bf861376f362e4a6fede967177
Omer Farooq <omr.frq1999@gmail.com>
2023-07-26 17:32:39 +0500

    made minor improvements

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request,render_template
+from flask import Flask, request,render_template, redirect
 from extract_metadata import extract_content_metadata
 from database import insert_and_index
 import shutil
@@ -9,19 +9,25 @@ app=Flask(__name__)
 # Get current path
 path = os.getcwd()
 # file Upload
-UPLOAD_FOLDER = os.path.join(path, 'uploads')
+UPLOAD = os.path.join(path, 'uploads')
+UPLOAD_FOLDER = os.path.join(path, 'files')
 
-# Make directory if uploads does not exists
+# Make directory if files folder do not exists
 if not os.path.isdir(UPLOAD_FOLDER):
     os.mkdir(UPLOAD_FOLDER)
 
+app.config['UPLOAD'] = UPLOAD
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def upload_form():
+    #creating uploads folder
+    if not os.path.isdir(UPLOAD):
+        os.mkdir(UPLOAD)
     return render_template('upload.html')
 
-@app.route('/upload', methods=['POST'])
+
+@app.route('/', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
   
@@ -30,6 +36,7 @@ def upload_file():
   
         # Iterate for each file in the files List, and Save them
         for file in files:
+            file.save(os.path.join(app.config['UPLOAD'], file.filename))
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
 
         #extracting metadata
@@ -42,13 +49,13 @@ def upload_file():
         #inserting into MongoDB and indexing
         insert_and_index(content, metadata)
 
-        #deleting upload
+        #deleting uploads
         try:
             shutil.rmtree(folder)
         except OSError as e:
             print(f"Error: {e}")
 
-        return "<h1>Files Uploaded Successfully.!</h1>"
+        return redirect('/')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port=5000,debug=False,threaded=True)
